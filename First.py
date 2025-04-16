68a2139371aded3dd6756b747e3b57efa71a0f9b
manirokni84 <manirokni25@gmail.com>
2025-04-17 03:07:19 +0330

    Delete 2.py

--- First.py
+++ First.py
@@ -18,3 +18,23 @@ print(n)
 
 m = a**b
 print(m)
+
+
+grades = []
+
+# گرفتن نمره 6 درس
+for i in range(6):
+    grade = float(input(f"نمره درس {i + 1} را وارد کنید: "))
+    grades.append(grade)
+
+# محاسبه معدل
+average = sum(grades) / len(grades)
+print(f"معدل شما: {average}")
+
+# بررسی شرایط جایزه
+if 18 <= average <= 20:
+    print("شما برنده پلی استیشن شدید!")
+elif 16 <= average < 18:
+    print("شما برنده دوچرخه شدید!")
+else:
+    print("شرمنده، جایزه‌ای نداریم!")
