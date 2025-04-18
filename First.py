39afebf6431c4a2cde9afb0a69db1db824207dcd
manirokni84 <manirokni25@gmail.com>
2025-04-18 10:04:03 +0330

    Update First.py

--- First.py
+++ First.py
@@ -38,3 +38,48 @@ elif 16 <= average < 18:
     print("شما برنده دوچرخه شدید!")
 else:
     print("شرمنده، جایزه‌ای نداریم!")
+
+
+
+
+country = input("where is your favorite county?")
+
+if country.lower()=="iran":
+    print("shiraz city")
+elif country.lower()=="france":
+    print("paris city")
+elif country.lower()=="england":
+    print("london city")
+else:
+    print("i dont know.")
+
+
+
+numbers = []
+
+for i in range(6):
+    num = float(input(f"عدد {i+1} را وارد کنید: "))
+    numbers.append(num)
+
+total_sum = sum(numbers)
+total_sub = numbers[0] - sum(numbers[1:])
+total_mul = 1
+for num in numbers:
+    total_mul *= num
+total_div = numbers[0]
+for num in numbers[1:]:
+    if num != 0:
+        total_div /= num
+    else:
+        total_div = "خطا: تقسیم بر صفر"
+        break
+
+print("جمع:", total_sum)
+print("تفریق:", total_sub)
+print("ضرب:", total_mul)
+print("تقسیم:", total_div)
+
+if isinstance(total_sum, (int, float)) and total_sum > 100:
+    print("Yes")
+else:
+    print("No")
